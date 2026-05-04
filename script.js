f6bfb7ccc6efd9b49cf19117862849fba08fdc6a
Ramesh <drlngramesh@gmail.com>
2026-05-04 16:55:43 +0530

    Added local storage support

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-console.log("App started");
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function renderTasks() {
+  const list = document.getElementById("taskList");
+  list.innerHTML = "";
+
+  tasks.forEach((task, index) => {
+    const li = document.createElement("li");
+    li.textContent = task;
+
+    li.onclick = () => {
+      tasks.splice(index, 1);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      renderTasks();
+    };
+
+    list.appendChild(li);
+  });
+}
+
 function addTask() {
   const input = document.getElementById("taskInput");
-  const list = document.getElementById("taskList");
 
-  const li = document.createElement("li");
-  li.textContent = input.value;
+  tasks.push(input.value);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 
-  list.appendChild(li);
   input.value = "";
+  renderTasks();
 }
+
+window.onload = renderTasks;
