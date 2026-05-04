5348da9e83144a145b26a3347a6cd52f20dc6db8
Ramesh <drlngramesh@gmail.com>
2026-05-04 16:47:40 +0530

    Add delete feature

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("App started");
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const list = document.getElementById("taskList");
+
+  const li = document.createElement("li");
+  li.textContent = input.value;
+
+  li.onclick = () => li.remove();
+
+  list.appendChild(li);
+  input.value = "";
+}
